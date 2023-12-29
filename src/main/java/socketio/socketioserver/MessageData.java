96db11fd1cd17858777aa51ba392ccaa7138a6c7
Sung Hyobin <hellmir610@gmail.com>
2023-12-29 22:34:11 +0900

    Revert "feat: Change type of isMine field from boolean to string (#1)"
    This reverts commit a0c8febcc9f6691ffd73bc6150a8690de5f4adae.

--- src/main/java/socketio/socketioserver/MessageData.java
+++ src/main/java/socketio/socketioserver/MessageData.java
@@ -2,12 +2,12 @@ package socketio.socketioserver;
 
 public class MessageData {
     private String inputMessage;
-    private String isMine;
+    private boolean isMine;
 
     public MessageData() {
     }
 
-    public MessageData(String inputMessage, String isMine) {
+    public MessageData(String inputMessage, boolean isMine) {
         this.inputMessage = inputMessage;
         this.isMine = isMine;
     }
@@ -16,7 +16,7 @@ public class MessageData {
         return inputMessage;
     }
 
-    public String isMine() {
+    public boolean isMine() {
         return isMine;
     }
 }
