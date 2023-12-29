2ebb111c5bdad161d8d5ca71f100fa8bc408d9e3
Sung Hyobin <hellmir610@gmail.com>
2023-12-29 19:30:26 +0900

    chore: Add logs and exception handling (#1)

--- src/main/java/socketio/socketioserver/controller/SocketIoController.java
+++ src/main/java/socketio/socketioserver/controller/SocketIoController.java
@@ -10,15 +10,20 @@ import socketio.socketioserver.message.MessageData;
 public class SocketIoController {
     private static final Logger log = LoggerFactory.getLogger(SocketIoController.class);
 
-    @SocketMapping(endpoint = "send_message", requestCls = MessageData.class)
+    private static final String SEND_MESSAGE = "send_message";
+    private static final String RECEIVE_MESSAGE = "receive_message";
+
+    private static final String RECEIVED_MESSAGE_LOG = "Received message from client [{}]: {}";
+    private static final String BROADCASTING_LOG = "Broadcasting message: {}";
+
+    @SocketMapping(endpoint = SEND_MESSAGE, requestCls = MessageData.class)
     public void onSendMessage(SocketIOClient client, MessageData data) {
-        log.info("Received message from client [{}]: {}", client.getSessionId(), data.getInputMessage());
-        // 추가적인 처리 로직
+        log.info(RECEIVED_MESSAGE_LOG, client.getSessionId(), data.getInputMessage());
     }
 
-    @SocketMapping(endpoint = "receive_message", requestCls = MessageData.class)
+    @SocketMapping(endpoint = RECEIVE_MESSAGE, requestCls = MessageData.class)
     public void onReceiveMessage(SocketIOClient client, MessageData data, SocketIOServer server) {
-        log.info("Broadcasting message: {}", data.getInputMessage());
-        server.getBroadcastOperations().sendEvent("receive_message", data);
+        log.info(BROADCASTING_LOG, data.getInputMessage());
+        server.getBroadcastOperations().sendEvent(RECEIVE_MESSAGE, data);
     }
 }
--- src/main/java/socketio/socketioserver/support/WebSocketAddMappingSupporter.java
+++ src/main/java/socketio/socketioserver/support/WebSocketAddMappingSupporter.java
@@ -3,6 +3,8 @@ package socketio.socketioserver.support;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
 import socketio.socketioserver.controller.SocketController;
@@ -17,9 +19,15 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Component
 public class WebSocketAddMappingSupporter {
-
     private final ConfigurableListableBeanFactory beanFactory;
     private SocketIOServer socketIOServer;
+
+    private static final Logger log = LoggerFactory.getLogger(WebSocketAddMappingSupporter.class);
+
+    private static final String ADD_LISTENERS_LOG = "Adding listeners for SocketControllers";
+    private static final String ADD_LISTENERS_FOR_CONTROLLER_LOG = "Adding event listeners for controller: {}";
+    private static final String REGISTERING_LOG = "Registering method {} for endpoint '{}'";
+    private static final String ERROR_LOG = "Error invoking method: {}";
 
     public void addListeners(SocketIOServer socketIOServer) {
         this.socketIOServer = socketIOServer;
@@ -27,27 +35,36 @@ public class WebSocketAddMappingSupporter {
                 .stream().map(Object::getClass)
                 .collect(Collectors.toList());
 
+        log.info(ADD_LISTENERS_LOG);
+
         for (Class<?> cls : classes) {
             List<Method> methods = findSocketMappingAnnotatedMethods(cls);
             addSocketServerEventListener(cls, methods);
         }
-
     }
 
     private void addSocketServerEventListener(Class<?> controller, List<Method> methods) {
+        log.info(ADD_LISTENERS_FOR_CONTROLLER_LOG, controller.getName());
+
         for (Method method : methods) {
             SocketMapping socketMapping = method.getAnnotation(SocketMapping.class);
             String endpoint = socketMapping.endpoint();
             Class<?> dtoClass = socketMapping.requestCls();
 
+            log.info(REGISTERING_LOG, method.getName(), endpoint);
+
             socketIOServer.addEventListener(endpoint, dtoClass, ((client, data, ackSender) -> {
-                List<Object> args = new ArrayList<>();
-                for (Class<?> params : method.getParameterTypes()) {
-                    if (params.equals(SocketIOServer.class)) args.add(socketIOServer);
-                    else if (params.equals(SocketIOClient.class)) args.add(client);
-                    else if (params.equals(dtoClass)) args.add(data);
+                try {
+                    List<Object> args = new ArrayList<>();
+                    for (Class<?> params : method.getParameterTypes()) {
+                        if (params.equals(SocketIOServer.class)) args.add(socketIOServer);
+                        else if (params.equals(SocketIOClient.class)) args.add(client);
+                        else if (params.equals(dtoClass)) args.add(data);
+                    }
+                    method.invoke(beanFactory.getBean(controller), args.toArray());
+                } catch (Exception e) {
+                    log.error(ERROR_LOG, method.getName(), e);
                 }
-                method.invoke(beanFactory.getBean(controller), args.toArray());
             }));
         }
     }
