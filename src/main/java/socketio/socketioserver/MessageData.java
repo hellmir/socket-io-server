39dafb0d79f0e26ee01d69fc782c502012fb0c0e
Sung Hyobin <hellmir610@gmail.com>
2023-12-29 23:14:11 +0900

    Revert "feat: Remove isMine field from MessageData class and change to receive only message  (#1)"
    This reverts commit 87d4a5b49d6c302fca5197dcc4fdba156ea1a4f4.

--- src/main/java/socketio/socketioserver/MessageData.java
+++ src/main/java/socketio/socketioserver/MessageData.java
@@ -2,11 +2,16 @@ package socketio.socketioserver;
 
 public class MessageData {
     private String inputMessage;
+    private boolean isMine;
 
     public MessageData() {
     }
 
     public String getInputMessage() {
         return inputMessage;
+    }
+
+    public boolean isMine() {
+        return isMine;
     }
 }
